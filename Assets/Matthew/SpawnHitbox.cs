4c70c858d64e1e025be2e4cd681ab369f93ccdd8
MUKILTEO\1726317 <bobby.bobbyjoejoejoe@gmail.com>
2025-10-13 10:20:47 -0700

    attack

---
+++ Assets/Matthew/SpawnHitbox.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnHitbox : MonoBehaviour
+{
+    public float attackRadius = 1.5f;
+    public LayerMask attackLayer;
+
+    private PlayerController playerController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    public void Attack2(InputAction.CallbackContext ctx)
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position + (Vector3)playerController.direction, attackRadius, Vector2.zero, 0, attackLayer);
+
+        if (hit)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position + (Vector3)playerController.direction, attackRadius);
+    }
+}
